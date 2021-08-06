d889c1820a6168c8ceee08b8f6866261a8ce5510
codwin2506 <siddharthb2506@gmail.com>
2021-08-06 15:22:25 +0530

    First commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "OOPs1"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/Assignment/MainObject.scala
@@ -0,0 +1,60 @@
+package Assignment
+
+import java.util.Scanner
+import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks.{break, breakable}
+
+object MainObject {
+  def main(args: Array[String]): Unit = {
+
+
+    val queue = new ListBuffer[Int]
+    breakable {
+      while (true) {
+        println("\nPress")
+        println("1. Insert number to Double it.")
+        println("2. Insert to Square the number.")
+        println("3. To Delete the element")
+        println("4. Display the OOPs Queue..")
+        println("5. Exit")
+        val sc = new Scanner(System.in)
+        val choice = sc.nextInt()
+        choice match {
+          case 1 =>
+            println("\nEnter the Element to be Pushed")
+            val elementToBePushed = sc.nextInt()
+            val queueObj = new DoubleQueue(queue)
+            queueObj.enqueue(elementToBePushed)
+
+          case 2 =>
+            println("\nEnter the Element to be Pushed")
+            val elementToBePushed = sc.nextInt()
+            val queueObj = new SquareQueue(queue)
+            queueObj.enqueue(elementToBePushed)
+          case 3 =>
+
+            if (queue.size == 0) println("\nEmpty OOPS.assign.Queue")
+            else {
+              val obj = new SquareQueue(queue)
+              val poppedElement = obj.dequeue(queue)
+              println("Deleted Element is : " + poppedElement)
+            }
+          case 4 => 1
+            if (queue.size == 0) println("\nEmpty OOPS.assign.Queue")
+            else {
+              println("\n OOPS.assign.Queue")
+              for (loopVariable <- queue)
+                print(loopVariable + " ")
+              println()
+
+            }
+          case 5 =>
+            println("Exiting")
+            break
+          case _ =>
+            println("Wrong Choice. Enter again")
+        }
+      }
+    }
+  }
+}
